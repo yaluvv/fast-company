aa41f5623078a4669635376abe0d6363397397aa
yaluvv <yaluvv@yandex.ru>
2023-02-16 15:10:53 +0300

    added pagination component

--- src/Components/TotalUsersTitle.jsx
+++ src/Components/TotalUsersTitle.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const TotalUsersTitle = ({ length }) => {
-  return (
-    <h1 className="p-3 mb-2 bg-dark text-white">{`Количество пользователей ${length}`}</h1>
-  );
+    return (
+        <h1 className="p-3 mb-2 bg-dark text-white">{`Количество пользователей ${length}`}</h1>
+    );
+};
+
+TotalUsersTitle.propTypes = {
+    length: PropTypes.number.isRequired
 };
 
 export default TotalUsersTitle;
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,41 +1,69 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import Table from "react-bootstrap/Table";
 import User from "./User";
+import AdvancedExample from "./Pagination";
+
+const pageSize = 4;
 
 const Users = ({ users, onDelete, onFavorite }) => {
-  if (!users.length) {
-    return <h1>Empty users</h1>;
-  }
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const itemsCount = users.length;
 
-  return (
-    <div>
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Имя</th>
-            <th>Качества</th>
-            <th>Профессия</th>
-            <th>Встретился раз</th>
-            <th>Оценка</th>
-            <th>Избранное</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            return (
-              <User
-                key={user._id}
-                {...user}
-                onDelete={onDelete}
-                onFavorite={onFavorite}
-              />
-            );
-          })}
-        </tbody>
-      </Table>
-    </div>
-  );
+    const paginate = (items, currentPage, pageSize) => {
+        const startIndex = (currentPage - 1) * pageSize;
+        return [...items].splice(startIndex, pageSize);
+    };
+
+    const usersDivided = paginate(users, currentPage, pageSize);
+
+    const handleChangePage = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    if (!itemsCount) {
+        return <h1>Empty users</h1>;
+    }
+
+    return (
+        <div>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Имя</th>
+                        <th>Качества</th>
+                        <th>Профессия</th>
+                        <th>Встретился раз</th>
+                        <th>Оценка</th>
+                        <th>Избранное</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usersDivided.map((user) => {
+                        return (
+                            <User
+                                key={user._id}
+                                {...user}
+                                onDelete={onDelete}
+                                onFavorite={onFavorite}
+                            />
+                        );
+                    })}
+                </tbody>
+            </Table>
+            <AdvancedExample
+                pageSize={pageSize}
+                itemsCount={itemsCount}
+                currentPage={currentPage}
+                onChangePage={handleChangePage}
+            />
+        </div>
+    );
+};
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onFavorite: PropTypes.func.isRequired
 };
 
 export default Users;
